f3bc64c38dde4d20b616c08eed24a6dc2e6a1ede
jaiba147 <felipehiba01@gmial.com>
2024-10-01 11:28:20 -0300

    pushing abstract class and interface with updated git ignore

---
+++ DM/src/main/java/ar/edu/itba/ss/IntegrationMethod.java
@@ -0,0 +1,9 @@
+package ar.edu.itba.ss;
+
+public interface IntegrationMethod {
+
+    void initializeValues();
+
+    Double calculate();
+
+}
---
+++ DM/src/main/java/ar/edu/itba/ss/Oscillator.java
@@ -0,0 +1,91 @@
+package ar.edu.itba.ss;
+
+public abstract class Oscillator {
+
+    private Double m;
+    private Double k;
+    private Double gamma;
+
+    private Double r;
+    private Double v;
+    private Double a;
+
+    private Double t;
+    private Double dt;
+
+    public Oscillator(Double m, Double k, Double gamma, Double r, Double v, Double a, Double t, Double dt) {
+        this.m = m;
+        this.k = k;
+        this.gamma = gamma;
+        this.r = r;
+        this.v = v;
+        this.a = a;
+        this.t = t;
+        this.dt = dt;
+    }
+
+    public Double getM() {
+        return m;
+    }
+
+    public void setM(Double m) {
+        this.m = m;
+    }
+
+    public Double getK() {
+        return k;
+    }
+
+    public void setK(Double k) {
+        this.k = k;
+    }
+
+    public Double getGamma() {
+        return gamma;
+    }
+
+    public void setGamma(Double gamma) {
+        this.gamma = gamma;
+    }
+
+    public Double getR() {
+        return r;
+    }
+
+    public void setR(Double r) {
+        this.r = r;
+    }
+
+    public Double getV() {
+        return v;
+    }
+
+    public void setV(Double v) {
+        this.v = v;
+    }
+
+    public Double getA() {
+        return a;
+    }
+
+    public void setA(Double a) {
+        this.a = a;
+    }
+
+    public Double getT() {
+        return t;
+    }
+
+    public void setT(Double t) {
+        this.t = t;
+    }
+
+    public Double getDt() {
+        return dt;
+    }
+
+    public void setDt(Double dt) {
+        this.dt = dt;
+    }
+
+}
